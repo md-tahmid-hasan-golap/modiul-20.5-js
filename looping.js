557fcfcf880cd27cd694c0e2e3ef768d6b9cfc05
golap-raj <golapraj47@gmail.com>
2025-02-10 03:15:29 -0600

    add

---
+++ looping.js
@@ -0,0 +1,25 @@
+// const friends = ['tahimd', 'hasan', 'golap', 'raj'];
+// // for(let friend of friends){
+// //     console.log(friend)
+// // }
+// for(let i = 0; i < friends.length; i++){
+//     console.log(i)
+//     console.log(friends[i])
+// }
+
+const numbers = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
+// console.log(numbers)
+// for(let number of numbers){
+//     console.log(number)
+// }
+// for(let i = 0; i < numbers.length; i++){
+//     console.log(i);
+//     console.log(numbers[i])
+// }
+
+let i = 0;
+while(i < numbers.length){
+    console.log(i);
+    console.log(numbers[i])
+    i++;
+}
